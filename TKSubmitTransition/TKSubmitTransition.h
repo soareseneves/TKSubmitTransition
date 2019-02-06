2a04d8b9ce68db45715c02ea8ea69184c2472f48
Douglas Soares <douglas.srs12@gmail.com>
2019-02-06 12:05:23 -0200

    Carthage Support

---
+++ TKSubmitTransition/TKSubmitTransition.h
@@ -0,0 +1,19 @@
+//
+//  TKSubmitTransition.h
+//  TKSubmitTransition
+//
+//  Created by Douglas Soares on 06/02/19.
+//  Copyright © 2019 Uniface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TKSubmitTransition.
+FOUNDATION_EXPORT double TKSubmitTransitionVersionNumber;
+
+//! Project version string for TKSubmitTransition.
+FOUNDATION_EXPORT const unsigned char TKSubmitTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TKSubmitTransition/PublicHeader.h>
+
+
